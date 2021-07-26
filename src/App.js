fb761bd3d2485b7852ec52b52266c166d1f125c1
gouravthakur39 <gourav.thakur39@gmail.com>
2021-07-26 23:52:16 +0530

    added Task.js, AddTask.js, ProjectOverlay.js, TaskDate.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { Fragment } from "react";
-import { Content } from "./components/layout/Content";
-import { Header } from "./components/layout/Header";
-import { ProjectsProvider, SelectedProjectProvider } from "./context";
-import "./App.scss";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
+import { ProjectsProvider, SelectedProjectProvider } from './context';
+import './App.scss'
 
-export const App = () => {
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
+
   return (
     <SelectedProjectProvider>
       <ProjectsProvider>
-        <Fragment>
-          <div className="App">
-            <Header />
-            <Content />
-          </div>
-        </Fragment>
+        <main
+          data-testid="application"
+          className={darkMode ? 'darkmode' : undefined}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Content />
+        </main>
       </ProjectsProvider>
     </SelectedProjectProvider>
   );
+};
+
+App.propTypes = {
+  darkModeDefault: PropTypes.bool,
 };
